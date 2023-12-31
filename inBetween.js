a3fa7e1a7413ad397403f3e8425e0d658c11a716
Kevin Aristhomene <swoledevkev@gmail.com>
2023-12-30 19:57:03 -0500

    codeWars 12/30/2023

---
+++ inBetween.js
@@ -0,0 +1,14 @@
+// instructions
+
+// Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+
+
+//ANSWER
+
+function between(a, b) {
+    const result = []
+    for (let i=a; i<=b; i++){
+      result.push(i)
+    }
+    return result
+  }
