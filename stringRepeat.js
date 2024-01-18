440229d22576877593f32e6df9318f0f73951b88
Kevin Aristhomene <swoledevkev@gmail.com>
2024-01-17 23:18:21 -0500

    codeWars 01/17/2024

---
+++ stringRepeat.js
@@ -0,0 +1,8 @@
+// Instructions
+
+/* Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+ */
+// Answer
+
+const repeatStr = (n, s) => s.repeat(n)
